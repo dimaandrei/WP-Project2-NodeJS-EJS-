f34a976922722ed43959e58e94fc31eb4be66924
Dima Raul Andrei <ix_andrei@andrei.dima>
2021-05-11 11:31:43 +0300

    Lab 11 p1+p2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
+const cookieParser=require('cookie-parser');
 const app = express();
-
 const port = 6789;
 const fs = require('fs');
 const { Console } = require('console');
@@ -17,11 +17,21 @@ app.use(express.static('public'))
 app.use(bodyParser.json());
 // utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser())
+
 
 // la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
-app.get('/', (req, res) => res.send('Hello World'));
+//app.get('/', (req, res) => res.send('Hello World'));
+
+app.get('/', (req, res) => {
+	// în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebări
+	
+	res.render('index', {
+		
+	});
+});
 
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
 app.get('/chestionar', (req, res) => {
@@ -59,8 +69,28 @@ app.post('/rezultat-chestionar', (req, res) => {
 	});
 });
 
+
+app.get('/autentificare', (req, res)=>{
+	res.render('autentificare', {
+        title: 'Autentificare', 
+    });    
+});
+
+app.post('/verificare-autentificare', (req, res) =>{
+	console.log(req.body);
+	//res.send("formular: " + JSON.stringify(req.body));
+	if(req.body.username==="test" && req.body.password==="test")
+	{
+		console.log("Corect");
+		req.cookies.username = req.body.username;
+		res.redirect('/');
+	}
+	
+	
+});
+
 //res.send("formular: " + JSON.stringify(req.body));
 
 
-app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
+app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:6789`));
 
